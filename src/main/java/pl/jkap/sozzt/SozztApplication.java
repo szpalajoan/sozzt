39252e847f9298c064f2bbbd5fb76f7eb4ff6059
jkap <blackish65@gmail.com>
2022-03-13 18:44:03 +0100

    first commit

---
+++ src/main/java/pl/jkap/sozzt/SozztApplication.java
@@ -0,0 +1,13 @@
+package pl.jkap.sozzt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SozztApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SozztApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jkap/sozzt/SozztApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jkap.sozzt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SozztApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
