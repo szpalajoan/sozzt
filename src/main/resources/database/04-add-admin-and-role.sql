f97288030a5e15cfc46391a9ecf8e7d1517b2680
jkap <blackish65@gmail.com>
2022-08-20 18:12:58 +0200

    fix sample data

--- src/main/resources/database/04-add-admin-and-role.sql
+++ src/main/resources/database/04-add-admin-and-role.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset jkap:4
 INSERT INTO users (id, username, password, enabled)
-VALUES (1, 'test', '$10$upzXFsFUOClFRR69OMKF8eajGMRs0vhcSHqvNDKy9yfW45w7o9z6O', 1);
+VALUES (1, 'test', '{bcrypt}$2a$10$upzXFsFUOClFRR69OMKF8eajGMRs0vhcSHqvNDKy9yfW45w7o9z6O', 1);
 
 INSERT INTO authorities (username, authority)
 VALUES ('test', 'ROLE_USER');
