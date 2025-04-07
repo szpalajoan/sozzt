5bfbd18f08e3bfce7ebd2a92ca53b59cccd0e21f
jKap <blackish65@gmail.com>
2025-04-07 21:06:45 +0200

    remart type nowe i bean dla land extracts

--- src/main/java/pl/jkap/sozzt/landextracts/domain/LandExtractsConfiguration.java
+++ src/main/java/pl/jkap/sozzt/landextracts/domain/LandExtractsConfiguration.java
@@ -1,11 +1,15 @@
 package pl.jkap.sozzt.landextracts.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import pl.jkap.sozzt.instant.InstantProvider;
 
+@Configuration
 @RequiredArgsConstructor
 public class LandExtractsConfiguration {
 
+    @Bean
     public LandExtractsFacade landExtractsFacade(InstantProvider instantProvider, LandExtractsEventPublisher landExtractsEventPublisher) {
         InMemoryLandExtractsRepository landExtractsRepository = new InMemoryLandExtractsRepository();
         return new LandExtractsFacade(landExtractsRepository, landExtractsEventPublisher, instantProvider);
